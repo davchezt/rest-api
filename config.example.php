e1481a0eb743bd215d1a42fb0382564dd633301c
Leonardo DaVchezt <davchezt@gmail.com>
2021-01-31 12:32:55 +0700

    Updating config.example.php

--- config.example.php
+++ config.example.php
@@ -9,20 +9,20 @@ defined("__DAVCHEZT") or die("{ \"response\" : \"error 403\"}");
 
 return [
     'app' => [
-        'hash' => 'DaVchezt',
-        'debug' => true,
-        'log' => true,
-        'secret' => 'U0hiqmizT7repIgy3wX1uJv6R3T8YtskNcZmF7ClH2ajBtE4nF8WXQGNAw5b3VVe'
+        'hash'      => 'cYi1sVLQ5wzm6gG0Ne0o8rpgkC3K8GzWVoU8tMjpJwDWm4JTFhDNn34JxEUQnGsT', // generated using Helper::generateRandomString
+        'secret'    => 'U0hiqmizT7repIgy3wX1uJv6R3T8YtskNcZmF7ClH2ajBtE4nF8WXQGNAw5b3VVe', // generated using Helper::generateRandomString
+        'debug'     => true,
+        'log'       => true
     ],
     'db' => [
-        'dsn' => "mysql:host=localhost;dbname=dbname",
-        'dbu' => "dbusername",
-        'dbp' => "dbpassword"
+        'dsn'       => "mysql:host=localhost;dbname=dbname",
+        'dbu'       => "dbusername",
+        'dbp'       => "dbpassword"
     ],
     'mail' => [
-        'host' => 'mail.domain.com',
-        'user' => 'raiza@domain.com',
-        'pass' => 'password',
-        'name' => 'Raiza Rhamdan'
+        'host'      => 'mail.domain.com',
+        'user'      => 'raiza@domain.com',
+        'pass'      => 'password',
+    'name'          => 'Raiza Rhamdan'
     ]
 ];
